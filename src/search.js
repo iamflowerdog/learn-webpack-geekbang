f7d746a2298daa4b2cd62f9abf7c7aa6eeca7eb2
yonghai.yang <yangyonghai9125@hotmail.com>
2019-10-14 14:49:24 +0800

    add webpack watch

--- src/search.js
+++ src/search.js
@@ -7,7 +7,7 @@ class Search extends React.Component {
     render () {
         return (
             <div className="search-text" >
-                <span className="text">中国汉字</span> 
+                <span className="text">中国汉字牛逼</span> 
                 <img src={ logo } />
             </div>
         )
