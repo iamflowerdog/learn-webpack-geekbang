92cebafea13f29f6b8a0fc3d2195550deae39f14
yonghai.yang <yangyonghai9125@hotmail.com>
2019-10-17 11:02:53 +0800

    use @babel/plugin-syntax-dynamic-import

--- src/search/index.js
+++ src/search/index.js
@@ -6,12 +6,27 @@ import './search.less';
 import logo from './image/logo.png';
 
 class Search extends React.Component {
+    constructor () {
+        super(...arguments);
+        this.state = {
+            Text: null
+        }
+    }
+    handleDynamicImport () {
+        import('./text.js').then((Text) => {
+            this.setState({
+                Text: Text.default
+            })
+        })
+    }
     render () {
+        const { Text } = this.state;
         return (
             <div className="search-text" >
                 { a() }
+                { Text ? <Text/> : null}
                 <span className="text">中国汉字牛逼666</span> 
-                <img src={ logo } />
+                <img src={ logo } onClick={this.handleDynamicImport.bind(this)}/>
             </div>
         )
     }
