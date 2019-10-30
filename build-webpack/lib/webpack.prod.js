6e5b35c695172c896c448b19f95a2ca34a62cc86
Mr.YanG <yangyh@epark.com>
2019-10-30 19:00:36 +0800

    add some files

--- build-webpack/lib/webpack.prod.js
+++ build-webpack/lib/webpack.prod.js
@@ -4,41 +4,40 @@ const baseConfig = require('./webpack.base');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 
-const devConfig = {
-    mode: 'production',
-    plugins: [
-        // webpack 3.0 可以直接使用 css-loader1.0 传入参数直接压缩，css-loader1.0后不支持这样做，需要安装
-        new OptimizeCSSAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano')
-        }),
-        new HtmlWebpackExternalsPlugin({
-            externals: [
-              {
-                module: 'react',
-                entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
-                global: 'React',
-              },
-              {
-                module: 'react-dom',
-                entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
-                global: 'ReactDOM',
-              },
-            ],
-          })
-    ],
-    optimization: {
-        splitChunks: {
-          minSize: 0,
-          cacheGroups: {
-            commons: {
-              name: 'vendors',
-              chunks: 'all',
-              minChunks: 2,
-            },
-          },
-        },
-    }
+const prodConfig = {
+	mode: 'production',
+	plugins: [
+		new OptimizeCSSAssetsPlugin({
+			assetNameRegExp: /\.css$/g,
+			cssProcessor: require('cssnano')
+		}),
+		new HtmlWebpackExternalsPlugin({
+			externals: [
+				{
+					module: 'react',
+					entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
+					global: 'React',
+				},
+				{
+					module: 'react-dom',
+					entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
+					global: 'ReactDOM',
+				},
+			],
+		})
+	],
+	optimization: {
+		splitChunks: {
+			minSize: 0,
+			cacheGroups: {
+				commons: {
+					name: 'vendors',
+					chunks: 'all',
+					minChunks: 2,
+				},
+			},
+		},
+	}
 }
 
-module.exports = merge(baseConfig, devConfig);
+module.exports = merge(baseConfig, prodConfig);
--- build-webpack/lib/webpack.ssr.js
+++ build-webpack/lib/webpack.ssr.js
@@ -1,135 +1,53 @@
-'use strict';
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-const glob = require('glob');
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
-
-const setMPA = () => {
-    const entry = {};
-    const htmlWebpackPlugins = [];
-    const entryFiles = glob.sync(path.join(__dirname, './src/*/index-server.js'));
-    // Users/yang/Desktop/my-project/src/index/index.js
-    
-    Object.keys(entryFiles)
-        .map((index) => {
-            const entryFile = entryFiles[index];
-            let match = entryFiles[index].match(/src\/(.*)\/index-server.js/);
-            let pageName = match && match[1];
-            
-            if (pageName) {
-                entry[pageName] = entryFile;
-                htmlWebpackPlugins.push(
-                    new HtmlWebpackPlugin({
-                        template: path.join(__dirname, `src/${pageName}/index.html`),
-                        filename: `${pageName}.html`,
-                        chunks: ['vendors', pageName],
-                        inject: true,
-                        minify: {
-                            html5: true,
-                            collapseWhitespace: true,
-                            preserveLineBreaks: false,
-                            minifyCSS: true,
-                            minifyJS: true,
-                            removeComments: false
-                        }
-                    })
-                )
-            }
-        });
-    return {
-        entry,
-        htmlWebpackPlugins
-    }
-}
-
-const { entry, htmlWebpackPlugins } = setMPA();
-
-module.exports = {
-    entry: entry,
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name]-server.js',
-        libraryTarget: 'umd'
-    },
+const ssrConfig = {
     mode: 'production',
     module: {
         rules: [
             {
-                test: /.js$/,
-                use: [
-                    'babel-loader',
-                    // 'eslint-loader'
-                ]
+                test: /\.css$/,
+                use: 'ignore-loader'
             },
             {
-                test: /.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [
-                                require('autoprefixer')({
-                                    browsers: ["last 2 version", ">1%", "IOS 7"]
-                                })
-                            ]
-                        }
-                    },
-                    {
-                        loader: 'px2rem-loader',
-                        options: {
-                            remUnit: 75,
-                            remPrecision: 8
-                        }
-                    },
-                    'less-loader',
-                ]
-            },
-            {
-                test: /.(png|jpg|jpeg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name]_[hash:8].[ext]'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /.(woff|woff2|eot|otf|ttf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name]_[hash:8][ext]'
-                        }
-                    }
-                ]
+                test: /\.less$/,
+                use: 'ignore-loader'
             }
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name]_[contenthash:8].css'
-        }),
+        // webpack 3.0 可以直接使用 css-loader1.0 传入参数直接压缩，css-loader1.0后不支持这样做，需要安装
         new OptimizeCSSAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano')
         }),
-        new CleanWebpackPlugin()
-    ].concat(htmlWebpackPlugins)
-};
+        new HtmlWebpackExternalsPlugin({
+            externals: [
+              {
+                module: 'react',
+                entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
+                global: 'React',
+              },
+              {
+                module: 'react-dom',
+                entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
+                global: 'ReactDOM',
+              },
+            ],
+          })
+    ],
+    optimization: {
+        splitChunks: {
+          minSize: 0,
+          cacheGroups: {
+            commons: {
+              name: 'vendors',
+              chunks: 'all',
+              minChunks: 2,
+            },
+          },
+        },
+    }
+}
+
+module.exports = merge(baseConfig, ssrConfig);
