c3476b4b38ab44b868c85bef237423b7ecffdf45
Mr.YanG <yangyh@epark.com>
2019-10-30 11:18:19 +0800

    add build-webpack

---
+++ build-webpack/lib/webpack.dev.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+const setMPA = () => {
+    const entry = {};
+    const htmlWebpackPlugins = [];
+    const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
+    // Users/yang/Desktop/my-project/src/index/index.js
+    
+    Object.keys(entryFiles)
+        .map((index) => {
+            const entryFile = entryFiles[index];
+            let match = entryFiles[index].match(/src\/(.*)\/index.js/);
+            let pageName = match && match[1];
+
+            entry[pageName] = entryFile;
+            htmlWebpackPlugins.push(
+                new HtmlWebpackPlugin({
+                    template: path.join(__dirname, `src/${pageName}/index.html`),
+                    filename: `${pageName}.html`,
+                    chunks: [pageName],
+                    inject: true,
+                    minify: {
+                        html5: true,
+                        collapseWhitespace: true,
+                        preserveLineBreaks: false,
+                        minifyCSS: true,
+                        minifyJS: true,
+                        removeComments: false
+                    }
+                })
+            )
+        });
+    return {
+        entry,
+        htmlWebpackPlugins
+    }
+}
+
+const { entry, htmlWebpackPlugins } = setMPA();
+module.exports = {
+    entry: entry,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /.(png|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240
+                        }
+                    }
+                ]
+            },
+            {
+                test: /.(woff|woff2|eot|otf|ttf)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            environment: JSON.stringify('dev')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new FriendlyErrorsWebpackPlugin()
+    ].concat(htmlWebpackPlugins),
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        stats: 'errors-only'
+    },
+    devtool: 'cheap-source-map'
+};
